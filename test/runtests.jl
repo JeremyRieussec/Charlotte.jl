0e790602295d2fc7bfbd0cd2b0cc76a3554095c7
JeremyRieussec <jeremy.rieussec@gmail.com>
2023-09-05 23:44:40 -0400

    First merge

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,7 +12,8 @@ end;
 ### Imprecise tests
 # As calculations on floating-point values can be imprecise, you can perform approximate equality checks 
 @testset "Imprecision test ----" begin
-    @test 1 ≈ 0.999999  rtol=1e-5
+    @test domath(1.0) ≈ 2.999999  rtol=1e-5
+    @test domath(2.0) ≈ 4.999999  rtol=1e-5
 end;
 
 ### Test sets
@@ -20,6 +21,4 @@ end;
     θ = 2/3*π
     @test sin(-θ) ≈ -sin(θ)
     @test cos(-θ) ≈ cos(θ)
-    @test sin(2θ) ≈ 2*sin(θ)*cos(θ)
-    @test cos(2θ) ≈ cos(θ)^2 - sin(θ)^2
 end;
