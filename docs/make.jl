e574c05e811d67d195b5e5d62b21b3ae5399c66e
JeremyRieussec <jeremy.rieussec@gmail.com>
2023-09-06 00:15:03 -0400

    Updtaing workflow

--- docs/make.jl
+++ docs/make.jl
@@ -11,6 +11,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/JeremyRieussec/Charlotte.git",
+    repo = "github.com/JeremyRieussec/Charlotte.jl.git",
     devbranch = "dev",
 )
